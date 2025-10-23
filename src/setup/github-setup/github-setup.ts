c494289a18ae36a494527ca5b08c462e1e7a9e44
Philip Bramwell <p.bramwell@pct-digital.de>
2025-10-23 06:20:17 +0200

    changes

--- src/setup/github-setup/github-setup.ts
+++ src/setup/github-setup/github-setup.ts
@@ -20,7 +20,7 @@ export async function postPullRequestReviewComments(reviews: ModelReviewsOutput[
       continue;
     }
 
-    await octokit.request(`POST /repos/${owner}/${repo}/pulls/${prNumber}/comments`, {
+    await octokit.request(`POST /repos/owner/repo/pulls/prNumber/comments`, {
       owner: owner,
       repo: repo,
       pull_number: prNumber,
